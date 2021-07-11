a09bbfbd659db18142ebc52671647b376db383d1
Ankit Pathak <78856709+ankitpathak530@users.noreply.github.com>
2021-07-11 17:51:08 +0530

    Update BigSorting_Hackerrank.java

--- BigSorting_Hackerrank.java
+++ BigSorting_Hackerrank.java
@@ -6,10 +6,9 @@ public class BigSorting {
     public static void main(String... args){
         String s[] = {"12","300","14534543526768546566787","98"};
         
-        ArrayList<Long> al = new ArrayList<Long>();
-        for(String ss:s){
-            long val=Long.parseLong(ss);
-            System.out.println(val);
-        }        
+        List<String> input = Arrays.asList(s);
+        input.sort(Comparator.comparing(a -> new BigInteger(a)));
+        System.out.println("After Sorting \n"+input);
+        
     }
 }
