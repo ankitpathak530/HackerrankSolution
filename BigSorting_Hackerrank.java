f0d1a4ba9c9826f61353bf466bd6b0d11c8c569a
Ankit Pathak <78856709+ankitpathak530@users.noreply.github.com>
2021-07-11 18:40:40 +0530

    Update BigSorting_Hackerrank.java

--- BigSorting_Hackerrank.java
+++ BigSorting_Hackerrank.java
@@ -5,10 +5,48 @@ import java.util.ArrayList;
 public class BigSorting {
     public static void main(String... args){
         String s[] = {"12","300","14534543526768546566787","98"};
-        
+        //1st Approach  4 test case pass
+        /*
         List<String> input = Arrays.asList(s);
         input.sort(Comparator.comparing(a -> new BigInteger(a)));
         System.out.println("After Sorting \n"+input);
+        
+        
+        //2nd Approach  6 Test cases pass
+        Collections.sort(input,new Comparator<String>() {
+        @Override
+           public int compare(String a, String b) {
+            // TODO Auto-generated method stub
+            if(a.length()>b.length())
+                return 1;
+              else if(a.length()<b.length())
+                return -1;
+              else if(Long.parseLong(a)>Long.parseLong(b))
+                 return 1;
+              else if(Long.parseLong(a)<Long.parseLong(b))
+                 return -1;
+              else
+                  return 0;
+        }});
+        */
+         Collections.sort(input,new Comparator<String>() {
+        @Override
+           public int compare(String a, String b) {
+            // TODO Auto-generated method stub
+              if(a.length()>b.length())
+                  return 1;
+              else if(a.length()<b.length())
+                  return -1;
+              else
+                {
+                   BigInteger b1 = new BigInteger(a);
+                   BigInteger b2 = new BigInteger(b);
+                   
+                  return b1.compareTo(b2);
+                }               
+        }});
+        return input;
+        
         
     }
 }
